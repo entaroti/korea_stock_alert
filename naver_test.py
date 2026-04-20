4300e57f4516dc2d3503a61a31cbdfb66d2b5262
entaroti <entaroti@gmail.com>
2026-04-20 16:28:42 +0900

    Update naver_test.py

--- naver_test.py
+++ naver_test.py
@@ -4,7 +4,7 @@ from io import StringIO
 
 print("🔥 START")
 
-url = "https://finance.naver.com/sise/sise_deal_rank.naver"
+url = "https://finance.naver.com/sise/sise_deal_rank.naver?sosok=0"
 
 headers = {
     "User-Agent": "Mozilla/5.0",
@@ -16,8 +16,7 @@ tables = pd.read_html(StringIO(res.text))
 
 print(f"총 테이블 개수: {len(tables)}")
 
-# 👉 모든 테이블 컬럼 확인
 for i, df in enumerate(tables):
     print(f"\n===== 테이블 {i} =====")
-    print(df.head(3))
+    print(df.head(5))
     print("컬럼:", df.columns)
